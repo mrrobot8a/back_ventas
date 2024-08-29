49d1260793d60e33c0e83bd829033822693edf5f
Jhon peralta <jhonperalta13@gmail.com>
2024-08-29 18:31:29 -0500

    chore: Add initial project files and dependencies

--- backend_ventas_api/models/sale_detail_model.py
+++ backend_ventas_api/models/sale_detail_model.py
@@ -9,16 +9,11 @@ def get_product_model():
     return Product
 
 class SaleDetail(models.Model):
-    id_sale = models.ForeignKey(get_sale_model(), on_delete=models.CASCADE, related_name='sale_details')
-    id_product = models.ForeignKey(get_product_model(), on_delete=models.CASCADE, related_name='product_details')
+    id_product = models.ForeignKey(get_product_model(), on_delete=models.CASCADE, related_name='sale_details')
     quantity = models.IntegerField()
-
+     
     class Meta:
-        unique_together = ('id_sale', 'id_product')  # Ensures that a product is only listed once per sale
-        constraints = [
-            models.UniqueConstraint(fields=['id_sale', 'id_product'], name='unique_product_sale')
-        ]
         db_table = 'sale_details'
 
     def __str__(self):
-        return f"{self.id_product} ({self.quantity}) in Sale {self.id_sale}"
+        return f"{self.id_product} ({self.quantity})"
--- backend_ventas_api/models/sale_model.py
+++ backend_ventas_api/models/sale_model.py
@@ -1,13 +1,22 @@
 from django.db import models
-from .customer_model import Customer  # Adjust import based on the location of the Customer model
+from .customer_model import Customer
+from .sale_detail_model import SaleDetail
 
 class Sale(models.Model):
     id_sale = models.AutoField(primary_key=True)
     id_customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True, related_name='sales')
+    id_sale_detail = models.ForeignKey(SaleDetail, on_delete=models.CASCADE, related_name='sales_details')
+    total = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)  # Se puede calcular automáticamente
     sale_date = models.DateField()
 
     def __str__(self):
         return f"Sale {self.id_sale} by {self.id_customer}"
+
+    def calculate_total(self):
+        total = sum(detail.quantity * detail.id_product.unit_price for detail in self.sales_details.all())
+        self.total = total
+        self.save()
+        return total
 
     class Meta:
         db_table = 'sales'
